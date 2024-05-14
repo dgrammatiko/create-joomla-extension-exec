e306f615e1f7a4fd0cb29a1cbbbd3566bfab6d61
Dimitris Grammatikogiannis <dg@dgrammatiko.dev>
2024-05-14 20:47:41 +0300

    use the github releases

--- build.mjs
+++ build.mjs
@@ -29,7 +29,7 @@ const pkg = await Bun.file("package.json").json();
 for (const os in builds) {
 	for (const build of builds[os]) {
 		console.log(os, build.cpu);
-		await $`bun build --compile --target=bun-${os}-${build.cpu} ./index.mjs --outfile dist/${os}/${pkg.dependencies["@dgrammatiko/create-joomla-extension"]}/${build.cpu}/cje`; // v17.3.0
+		await $`bun build --compile --target=bun-${os}-${build.cpu} ./index.mjs --outfile dist/${os.toLowerCase()}_${pkg.dependencies["@dgrammatiko/create-joomla-extension"]}_${build.cpu}_cje`; // v17.3.0
 	}
 }
 
