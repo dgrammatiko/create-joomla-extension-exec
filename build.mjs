0bf607853efb8efdfbf0d396c426c8f256797ed7
Dimitris Grammatikogiannis <dg@dgrammatiko.dev>
2024-05-14 12:31:13 +0300

    0.0.4

--- build.mjs
+++ build.mjs
@@ -24,10 +24,12 @@ const builds = {
 	],
 };
 
+const pkg = await Bun.file("package.json").json();
+
 for (const os in builds) {
 	for (const build of builds[os]) {
-		console.log(os, build);
-		await $`bun build --compile --target=bun-${os}-${build.cpu} ./index.mjs --outfile dist/${os}/${build.cpu}/cje`; // v17.3.0
+		console.log(os, build.cpu);
+		await $`bun build --compile --target=bun-${os}-${build.cpu} ./index.mjs --outfile dist/${os}/${pkg.dependencies["@dgrammatiko/create-joomla-extension"]}/${build.cpu}/cje`; // v17.3.0
 	}
 }
 
